42d7c0455f9f626aa68892b17ed774bbebcd6c5f
PawelKonczalski <konczalski.pawel@gmail.com>
2019-07-10 14:56:24 +0200

    Add the service, servlet and DTO for the lang Add index site with welcome message in choose language for input data

--- src/main/java/pl/babel/lang/LangRepository.java
+++ src/main/java/pl/babel/lang/LangRepository.java
@@ -2,6 +2,7 @@ package pl.babel.lang;
 
 import pl.babel.HibernateUtil;
 
+import java.util.List;
 import java.util.Optional;
 
 public class LangRepository {
@@ -9,6 +10,15 @@ public class LangRepository {
         var session = HibernateUtil.getSessionFactory().openSession();
         var transaction = session.beginTransaction();
         var result = Optional.ofNullable(session.get(Lang.class, id));
+        transaction.commit();
+        session.close();
+        return result;
+    }
+
+    List<Lang> findAll() {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+        var result = session.createQuery("from Lang", Lang.class).list();
         transaction.commit();
         session.close();
         return result;
